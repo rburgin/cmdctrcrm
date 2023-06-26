0b82468a23798e615a9fb5128ca3a7c41aed9ccd
rburgin <r@st33.com>
2023-06-26 09:13:12 -0700

    Update and rename dynamic-js.js to functions.js

--- dynamic-js.js
+++
@@ -1,4 +0,0 @@
-<!-- ThemeBuilder Code Start -->
-<script id="themebuilder-script" src="https://builder.themarketerstoolkit.com/hlembed.js" data-agency-id="pi5b1lo1" ></script>
-<!-- ThemeBuilder Code End -->
-<script src="https://cdn.everypages.com/ghl-customizer/ghl_customizer.php?agency_id=pi5b1lo1"></script>
